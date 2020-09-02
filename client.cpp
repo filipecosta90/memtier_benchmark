d9bbc69c09e4409a39ddf1d1e224c3d9a09096ea
filipecosta90 <filipecosta.90@gmail.com>
2020-09-02 12:51:22 +0100

    [fix] fixes per key range on P mode

--- client.cpp
+++ client.cpp
@@ -72,29 +72,16 @@ bool client::setup_client(benchmark_config *config, abstract_protocol *protocol,
     else if (config->distinct_client_seed)
         m_obj_gen->set_random_seed(config->next_client_idx);
 
-    client_index = config->next_client_idx % total_num_of_clients;
-
     // Parallel key-pattern determined according to the first command
     if ((config->arbitrary_commands->is_defined() && config->arbitrary_commands->at(0).key_pattern == 'P') ||
         (config->key_pattern[key_pattern_set]=='P')) {
-        unsigned long long key_range_size = config->key_maximum - config->key_minimum;
-        if (config->key_minimum!=0){
-            key_range_size++;
-        }
         unsigned long long client_index = config->next_client_idx % total_num_of_clients;
-        unsigned long long range = key_range_size/total_num_of_clients+1;
+        unsigned long long range = (config->key_maximum - config->key_minimum)/total_num_of_clients + 1;
         unsigned long long min = config->key_minimum + (range * client_index);
-        unsigned long long max = min + range -1;
-        if (client_index == (total_num_of_clients - 1) || max > config->key_maximum) {
+        unsigned long long max = min + range - 1;
+        if (client_index == (total_num_of_clients - 1)) {
             max = config->key_maximum; //the last clients takes the leftover
         }
-        if ( (total_num_of_clients ) > key_range_size ){
-            benchmark_debug_log("total key range [ %lld , %lld ] is smaller than number of total clients %d.\n", config->key_minimum, config->key_maximum,total_num_of_clients );
-            range = 1;
-            min = config->key_minimum + (client_index % key_range_size);
-            max = min;
-        }
-        benchmark_debug_log("client_index %lld min-max [ %lld , %lld ].\n", client_index, min, max );
         m_obj_gen->set_key_range(min, max);
     }
     config->next_client_idx++;
